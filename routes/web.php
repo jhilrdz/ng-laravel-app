331be0a36f03890461e3cc6d31421142cdf6d5d4
Hilario Rodriguez <jhilrdz@hotmail.com>
2023-02-27 20:40:52 -0600

    Create Laravel View for Angular

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AngularController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
